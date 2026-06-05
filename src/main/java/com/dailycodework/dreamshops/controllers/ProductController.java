6d9640a4293d157ffab9e58cdcf941122ce5cfab
Antek <antoni.samol@gmail.com>
2026-06-05 13:14:22 +0200

    Product controller in progress

--- src/main/java/com/dailycodework/dreamshops/controllers/ProductController.java
+++ src/main/java/com/dailycodework/dreamshops/controllers/ProductController.java
@@ -1,4 +1,63 @@
 package com.dailycodework.dreamshops.controllers;
 
+
+import com.dailycodework.dreamshops.exception.ProductNotFoundException;
+import com.dailycodework.dreamshops.exception.ResourceNotFoundException;
+import com.dailycodework.dreamshops.model.Product;
+import com.dailycodework.dreamshops.request.AddProductRequest;
+import com.dailycodework.dreamshops.request.ProductUpdateRequest;
+import com.dailycodework.dreamshops.response.ApiResponse;
+import com.dailycodework.dreamshops.service.product.IProductService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
+import static org.springframework.http.HttpStatus.NOT_FOUND;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("${api.prefix}/products")
 public class ProductController {
+    private final IProductService productService;
+
+    @GetMapping("/all")
+    public ResponseEntity<ApiResponse> getAllProducts(){
+        List<Product> products = productService.getAllProducts();
+        return ResponseEntity.ok(new ApiResponse("success", products));
+    }
+
+    @GetMapping("/product/{productId}/product")
+    public ResponseEntity<ApiResponse> getProductById(@PathVariable Long productId){
+        try {
+            Product products = productService.getProductById(productId);
+            return ResponseEntity.ok(new ApiResponse("success", products));
+        } catch (ResourceNotFoundException e) {
+            return ResponseEntity.status(NOT_FOUND).body(new ApiResponse(e.getMessage(), null));
+        }
+    }
+
+    @PostMapping("/add")
+    public ResponseEntity<ApiResponse> addProduct(@RequestBody AddProductRequest product){
+        try {
+            Product theProduct = productService.addProduct(product);
+            return ResponseEntity.ok(new ApiResponse("Product added successfully", theProduct));
+        } catch (Exception e) {
+            return ResponseEntity.status(INTERNAL_SERVER_ERROR).body(new ApiResponse(e.getMessage(), null));
+        }
+    }
+
+    @PutMapping("/product/{productId}/update")
+    public ResponseEntity<ApiResponse> updateProduct(@RequestBody ProductUpdateRequest request, @PathVariable Long productId){
+        try {
+            Product theProduct = productService.updateProduct(request, productId);
+            return ResponseEntity.ok(new ApiResponse("Product updated successfully", theProduct));
+        } catch (ResourceNotFoundException e) {
+            return ResponseEntity.status(NOT_FOUND).body(new ApiResponse(e.getMessage(), null));
+    }
+}
+
+
 }
