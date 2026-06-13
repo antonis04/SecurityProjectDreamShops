cf92c7d9764f20411cf6a379496178b7a7c52193
Antek <antoni.samol@gmail.com>
2026-06-13 15:02:28 +0200

    SpringSecurity Implementation finished

--- src/main/java/com/dailycodework/dreamshops/controllers/CartItemController.java
+++ src/main/java/com/dailycodework/dreamshops/controllers/CartItemController.java
@@ -8,18 +8,19 @@ import com.dailycodework.dreamshops.response.ApiResponse;
 import com.dailycodework.dreamshops.service.cart.CartItemService;
 import com.dailycodework.dreamshops.service.cart.ICartService;
 import com.dailycodework.dreamshops.service.user.IUserService;
+import io.jsonwebtoken.JwtException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import static org.springframework.http.HttpStatus.NOT_FOUND;
+import static org.springframework.http.HttpStatus.UNAUTHORIZED;
 
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("${api.prefix}/cartItems")
 
 public class CartItemController {
-
     private final ICartService cartService;
     private final CartItemService cartItemService;
     private final IUserService userService;
@@ -29,13 +30,15 @@ public class CartItemController {
                                                      @RequestParam Long productId,
                                                      @RequestParam Integer quantity) {
         try {
-                User user = userService.getUserById(4L);
-                Cart cart = cartService.initializeNewCart(user);
+            User user = userService.getAuthenticatedUser();
+            Cart cart = cartService.initializeNewCart(user);
 
             cartItemService.addItemToCart(cart.getId(), productId, quantity);
             return ResponseEntity.ok(new ApiResponse("Added to cart successfully", null));
         } catch (ResourceNotFoundException e) {
             return ResponseEntity.status(NOT_FOUND).body(new ApiResponse(e.getMessage(), null));
+        } catch (JwtException e) {
+            return ResponseEntity.status(UNAUTHORIZED).body(new ApiResponse(e.getMessage(), null));
         }
     }
 
--- src/main/java/com/dailycodework/dreamshops/data/DataInitializer.java
+++ src/main/java/com/dailycodework/dreamshops/data/DataInitializer.java
@@ -1,36 +1,80 @@
 package com.dailycodework.dreamshops.data;
 
+import com.dailycodework.dreamshops.model.Role;
 import com.dailycodework.dreamshops.model.User;
+import com.dailycodework.dreamshops.repository.RoleRepository;
 import com.dailycodework.dreamshops.repository.UserRepository;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.ApplicationListener;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
+import java.util.Set;
+
+@Transactional
 @Component
 @RequiredArgsConstructor
 public class DataInitializer implements ApplicationListener<ApplicationReadyEvent> {
 
     private final UserRepository userRepository;
+    private final RoleRepository roleRepository;
+    private final PasswordEncoder passwordEncoder;
 
     @Override
     public void onApplicationEvent(ApplicationReadyEvent event) {
+        Set<String> defaultRoles = Set.of("ROLE_ADMIN", "ROLE_USER");
         createDefaultUserIfNotExist();
+        createDefaultRoleIfNotExist(defaultRoles);
+        createDefaultAdminIfNotExist();
     }
 
+
+
     private void createDefaultUserIfNotExist() {
+        Role userRole = roleRepository.findByRoleName("ROLE_USER").get();
         for (int i = 0; i < 5; i++) {
             String defaultEmail = "user" + i + "@email.com";
             if (userRepository.existsByEmail(defaultEmail)) {
                 continue;
             }
             User user = new User();
-            user.setFirstName("User");
+            user.setFirstName("The User");
             user.setLastName("User" + i);
             user.setEmail(defaultEmail);
-            user.setPassword("123456");
+            user.setPassword(passwordEncoder.encode("123456"));
+            user.setRoles(Set.of(userRole));
             userRepository.save(user);
-            System.out.println("Default vet user: " + i + " created");
+            System.out.println("Default user: " + i + " created");
         }
+    }
+
+
+    private void createDefaultAdminIfNotExist() {
+        Role adminRole = roleRepository.findByRoleName("ROLE_ADMIN").get();
+        for (int i = 0; i < 2; i++) {
+            String defaultEmail = "admin" + i + "@email.com";
+            if (userRepository.existsByEmail(defaultEmail)) {
+                continue;
+            }
+            User user = new User();
+            user.setFirstName("Admin");
+            user.setLastName("User" + i);
+            user.setEmail(defaultEmail);
+            user.setPassword(passwordEncoder.encode("123456"));
+            user.setRoles(Set.of(adminRole));
+            userRepository.save(user);
+            System.out.println("Default admin user: " + i + " created");
+        }
+    }
+
+
+
+
+    private void createDefaultRoleIfNotExist(Set<String> roles) {
+        roles.stream()
+                .filter(role -> roleRepository.findByRoleName(role).isEmpty())
+                .map(Role:: new) .forEach(roleRepository::save);
     }
 }
